70530834c7e2c0d9a0cbcc95facb7fc5e73cf7bf
OmarP26 <omar.perez@leftfieldlabs.com>
2022-02-24 10:37:52 -0800

    Finished Gatsby Tutorial Part 3

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,12 @@
 import * as React from 'react'
 import Layout from '../components/layout'
+import { StaticImage } from 'gatsby-plugin-image'
 
 const IndexPage = () => {
   return (
     <Layout pageTitle="Home Page">
       <p>I'm making this by following the Gatsby Tutorial!</p>
+      <StaticImage src="../images/green_field.jpg" alt="Empty green plain with a single tree in the middle."/>
     </Layout>
   )
 }
